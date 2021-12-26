df7126f21aca80c63b684710452f58df5ae5f62d
Sajid Mehmood <maliksajidawan786@gmail.com>
2021-12-26 19:58:51 +0500

    fix css changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,6 +42,7 @@
     <script>
         var swiper = new Swiper(".mySwiper", {
             direction: "vertical",
+            autoHeight: true,
             pagination: {
                 el: ".swiper-pagination",
                 clickable: true,
