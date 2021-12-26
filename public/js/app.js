6e84cc9b6880655c9fe8d53324e2448f1840cd51
Sajid Mehmood <maliksajidawan786@gmail.com>
2021-12-26 18:28:57 +0500

    change in code

--- public/js/app.js
+++ public/js/app.js
@@ -68413,8 +68413,8 @@ if (document.getElementById('app')) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\taro.com\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\taro.com\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\taro\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\taro\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
