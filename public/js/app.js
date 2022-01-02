e8522c6c31a77dbc9841e445c736c847af80d906
myownIdeas <muhammad.waqas7266@gmail.com>
2022-01-02 19:22:21 +0500

    changes in code

--- public/js/app.js
+++ public/js/app.js
@@ -68227,8 +68227,8 @@ if (document.getElementById('app')) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\taro\taro-app\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\taro\taro-app\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\taro\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\taro\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
